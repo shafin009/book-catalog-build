3d210eeae143e21079cc28dacb4984bc190a3708
shafin009 <ghostboyshafin.com>
2023-09-19 22:17:10 +0600

    token-separated

--- dist/app/modules/books/book-interface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BookFilterField = exports.BookSearchField = void 0;
-exports.BookSearchField = ['title', 'author', 'genre'];
-exports.BookFilterField = ['search', 'category', 'minPrice', 'maxPrice'];
